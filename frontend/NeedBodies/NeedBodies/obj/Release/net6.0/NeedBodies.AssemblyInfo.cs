e25bc184533c1cb3c4f2d669665b289a3a14fbb5
nic <bolton21@uwindsor.ca>
2024-06-05 12:05:56 -0400

    add more flexible sidebar and logo

--- frontend/NeedBodies/NeedBodies/obj/Release/net6.0/NeedBodies.AssemblyInfo.cs
+++ frontend/NeedBodies/NeedBodies/obj/Release/net6.0/NeedBodies.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
     "is a component library built on top of Blazor and CSS frameworks like Bootstrap," +
     " Tailwind, Bulma and Material.\r\n\t\t")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.3.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.3+84a796ef616486cc6052eeec6c5e11f4de9de991")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.3+95df3e462efd0adeb59c1acc69e230bb64e04540")]
 [assembly: System.Reflection.AssemblyProductAttribute("NeedBodies")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NeedBodies")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.3.0.0")]
